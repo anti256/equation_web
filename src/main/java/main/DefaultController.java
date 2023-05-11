68fef31fc51834f34349d6bc6c54091d15e9ff43
anti256 <anti256@mail.ru>
2023-05-11 16:33:51 +0300

    spring_start

--- src/main/java/main/DefaultController.java
+++ src/main/java/main/DefaultController.java
@@ -1,32 +1,87 @@
 package main;
 
-import main.model.Task;
-import main.model.TaskRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+//import main.model.Task;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.Date;
 
-@Controller
+@RestController
 public class DefaultController {
 
-    @Autowired
-    TaskRepository taskRepository;
+   // @Autowired
+   // TaskRepository taskRepository;
 
     @RequestMapping("/")
     public String index(Model model){
-        Iterable<Task> taskIterable = taskRepository.findAll();
-        ArrayList<Task> taskS = new ArrayList<>();
-        for (Task task : taskIterable){
-            taskS.add(task);
-        }
-        model.addAttribute("tasks", taskS);
-        model.addAttribute("tasksCount", taskS.size());
-        return "index";
-        //return (new Date()).toString();
+        //Iterable<Task> taskIterable = taskRepository.findAll();
+        //ArrayList<Task> taskS = new ArrayList<>();
+//        for (Task task : taskIterable){
+//            taskS.add(task);
+//        }
+//        model.addAttribute("tasks", taskS);
+//        model.addAttribute("tasksCount", taskS.size());
+//        return "index";
+        return (new Date()).toString();
     }
+//
+//    public static class Storage {
+//        private static volatile int currentId = 1;//начальное значение идентификатора
+//        private static HashMap<Integer, Task> tasks = new HashMap<Integer, Task>();//набор задач в виде HashMap
+//        private static int tasksCount;//общее количество задач
+//
+//        //возвращает общее количество задач
+//        public static int getTasksCount() {
+//            return tasksCount;
+//        }
+//
+//        //возвращает весь список задач - фактически переделывает HashMap задач в ArrayList
+//        public static synchronized List<Task> getAllTasks(){
+//            ArrayList<Task> taskList = new ArrayList<Task>();
+//            taskList.addAll(tasks.values());
+//            return taskList;
+//        }
+//
+//        //добавление задачи
+//        public static synchronized int addTask(Task task){//на вход поступает заготовка задачи без id  и date
+//            int id = currentId++;//переменной назначается значение, потом это значение увеличивается на единицу
+//            task.setId(id);//назначение задаче идентификатора
+//            SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yy HH.mm");
+//            task.setDate(dateFormat.format(new Date()));//назначение задаче даты и времени создания
+//            tasks.put(id, task);//внесение задачи в HashMap задач
+//            tasksCount = tasks.size();//расчет общего количества задач
+//            return id;
+//        }
+//
+//        //возврат отдельной задачи по id
+//        public static synchronized Task getTask(int taskId){
+//            if (tasks.containsKey(taskId)){//если id есть в списке
+//            return tasks.get(taskId);}//возвращается задача
+//            return null;//если id нет, возвращается null
+//        }
+//
+//        //удаление всех задач
+//        public static synchronized int delAllTasks(){
+//            tasks.clear();//очистка HashMap задач
+//            currentId = 1;//сброс текущего идентификатора
+//            return tasksCount = tasks.size();
+//        }
+//
+//        //удаление отдельной задачи
+//        public static synchronized int deleteOne(int taskId){
+//            tasks.remove(taskId);
+//            if (tasks.size() == 0){currentId = 1;}//если задач не осталось, сбрасываем текущий идентификатор в начальное значение
+//            return tasksCount = tasks.size();
+//        }
+//
+//        //изменение одной задачи
+//        public static synchronized int PutTask(Task task){//на входе нет id, т.к. он есть в задаче
+//            SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yy HH.mm");
+//            task.setDate(dateFormat.format(new Date()));//обновляем дату-время задачи
+//            tasks.put(task.getId(), task);//обновляем задачу в HashMap задач
+//            return task.getId();
+//        }
+//    }
+//
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,7 +1,87 @@
 package main;
 
+//import main.model.Task;
+//import main.model.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
+        // System.out.println("Hello world!");
     }
+
+//    @RestController
+//    public static class TaskController {
+//
+//        @Autowired//аннотация - автоматическое создание репозитория
+//        private TaskRepository taskRepository;
+//
+//        @GetMapping("/todos")//ссылка относительно главной страницы
+//        public List<Task> list(){
+//            Iterable<Task> taskIterable = taskRepository.findAll();
+//            ArrayList<Task> tasks = new ArrayList<>();
+//            for (Task task : taskIterable){
+//                tasks.add(task);
+//            }
+//            return tasks;//возвращает список задач
+//        }
+//
+//        @PostMapping("/todos")
+//        public int add(Task task){//добавление задачи
+//            SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yy HH.mm");
+//            task.setDate(dateFormat.format(new Date()));//назначение задаче даты и времени создания
+//            Task newTask = taskRepository.save(task);
+//            return newTask.getId();
+//        }
+//
+//        @GetMapping("/todos/{id}")
+//        public ResponseEntity get(@PathVariable int id){//раскрытие отдельной задачи
+//            Optional<Task> optionalTask = taskRepository.findById(id);
+//            if (!optionalTask.isPresent()) {
+//                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+//            }
+//            return new ResponseEntity(optionalTask.get(), HttpStatus.OK);
+//        }
+//
+//        @DeleteMapping("/todos")//удаление всех задач
+//        public int delete(){
+//            taskRepository.deleteAll();
+//            return (int) taskRepository.count();
+//        }
+//
+//        @DeleteMapping("/todos/{id}")//удаление отдельной задачи
+//        public int deleteOne(@PathVariable int id){
+//            taskRepository.deleteById(id);
+//            return (int) taskRepository.count();
+//        }
+//
+//        @PutMapping("/todos/{id}")//обновление отдельной задачи
+//        public int Put(@PathVariable int id, @RequestBody Task t){
+//            Task task = new Task();
+//            task.setId(t.getId());
+//            task.setName(t.getName());
+//            task.setTasktext(t.getTasktext());
+//            SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yy HH.mm");
+//            task.setDate(dateFormat.format(new Date()));//обновляем дату-время задачи
+//            taskRepository.save(task);
+//            return task.getId();
+//        }
+//
+//        @GetMapping("/todos/count")//общее количество задач
+//        public int get(){
+//            return (int) taskRepository.count();
+//        }
+//    }
 }
